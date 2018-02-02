590e6d5ba712e34ad2776b6862d8f97be6385d31
Bryan Brinson <fdfd@beb.com>
2018-02-01 20:21:44 -0800

    removed unused code

--- js/salesForecast.js
+++ js/salesForecast.js
@@ -3,7 +3,6 @@
 // hoursOfOperations are 6AM __to__ 8PM or 14 Hrs.
 const hoursOfOperations = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
 var stores = [];
-const btnSaveAndContinue = document.getElementById('saveAndContinue');
 const salesForecastTableId = document.getElementById('salesForecastTable');
 var tableBody = salesForecastTableId.createTBody();
 
